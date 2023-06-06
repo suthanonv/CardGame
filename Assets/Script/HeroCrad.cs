d8a8c2e4f28ca09fa626805189bc4bfd7b180d20
Penake Harntaweesompone <st1849@roong-aroon.ac.th>
2023-06-06 10:11:14 +0700

    add hero card scriptable object

---
+++ Assets/Script/HeroCrad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class HeroCrad : ScriptableObject
+{
+
+    public string Name;
+
+    public int SummonCost;
+
+    public float Force;
+
+    public int SkillCost;
+
+    public GameObject Skill;
+
+}
